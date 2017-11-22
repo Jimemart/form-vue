64bc9bfe23c3c57c4c803c3fd96986bc2cbcf8b6
Jimena Martin <jimena.martin@grupoonetec.com>
2017-11-22 09:40:05 +0100

    done

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,90 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <form>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <my-full-name @fullNameDone="userData.name = $event"></my-full-name>
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" name="email" class='form-control' v-model="userData.email">
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="password" class='form-control' v-model="userData.password">
+          </div>
+
+        </div>
+      </div>
+      <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <h4>Store Data?</h4>
+                    <label for="yes">
+                        <input
+                                type="radio"
+                                id="yes"
+                                value="Yes"
+                                v-model="storeMe"> Yes
+                    </label>
+                    <label for="no">
+                        <input
+                                type="radio"
+                                id="no"
+                                value="No"
+                                v-model="storeMe"> No
+                    </label>
+                </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+          <button class="btn btn-primary" @click.prevent="submited = true">Submit!</button>
+        </div>
+      </div>
+    </form>
+    <hr>
+    <div class="row" v-if="submited">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="cpanel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>Full name: {{userData.name}}</p>
+            <p>Mail: {{userData.email}}</p>
+            <p>Password: {{userData.password}}</p>
+            <p>Store in Database?: {{storeMe}} </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import FullName from './FullName.vue'
+
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  components:{
+    'my-full-name': FullName
+  },
+  data: ()=>{
+   return {
+     userData : {
+      name : '',
+      password : '',
+      email: ''
+      },
+      storeMe: '',
+      submited : false
+    }
+  },
+  methods:{
+    showme(val){
+      console.log(val)
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
---
+++ src/FullName.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+<div class="form-group">
+     <label for="name">First Name</label>
+     <input type="text" name="name" class='form-control' @input="createFull($event.target.value, true)" :value="userData.name">
+</div>
+<div class="form-group">
+     <label for="last">Last Name</label>
+     <input type="text" name="last" class='form-control' @input="createFull($event.target.value, false)" :value="userData.last">
+</div>
+</div>
+
+</template>
+
+<script>
+export default {
+    data (){
+        return {
+            userData:{
+                name: '',
+                last: ''
+            },
+            fullName: ''
+            
+        }
+    },
+    methods:{
+        sayHi(){
+           console.log('hola')
+
+        },
+        createFull(val, boo){
+            if(boo){
+                this.userData.name = val
+                this.fullName = val + ' ' + this.userData.last
+                console.log(this.userData)
+                this.sendInfo()
+            }else{
+                this.userData.last = val
+                
+                this.fullName = this.userData.name + ' ' + val
+                this.sendInfo()
+            }
+        },
+        sendInfo(){
+            
+            this.$emit('fullNameDone', this.fullName)
+        }
+    },
+    
+};
+</script>
+
+<style>
+
+</style>
